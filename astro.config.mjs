38a8fd6f3352bbb758b26d85f34ab227d92262ea
Gerard Albajar <gerard.albajar@gmail.com>
2025-04-09 13:19:54 +0200

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,14 @@
 import { defineConfig } from 'astro/config';
-import tailwindcss from '@tailwindcss/vite';
 
+import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
+import vercel from '@astrojs/vercel';
 
 export default defineConfig({
     vite: {
         plugins: [tailwindcss()]
     },
 
-    integrations: [react()]
+    integrations: [react()],
+    adapter: vercel({ webAnalytics })
 });
