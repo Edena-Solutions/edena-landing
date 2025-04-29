1749d5ed20335ffc91eff16d16135a64378e9683
Gerard Albajar <gerard.albajar@gmail.com>
2025-04-29 09:36:34 +0200

    Deploy Github Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,14 +2,11 @@ import { defineConfig } from 'astro/config';
 
 import tailwindcss from '@tailwindcss/vite';
 import react from '@astrojs/react';
-import vercel from '@astrojs/vercel';
 
 export default defineConfig({
+    site: 'https://www.edena.es',
     vite: {
         plugins: [tailwindcss()]
     },
-    integrations: [react()],
-    adapter: vercel({
-        analytics: true
-    })
+    integrations: [react()]
 });
