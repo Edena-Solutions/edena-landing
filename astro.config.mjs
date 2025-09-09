2f8ee17ed61acf818ba74fb980c7ef84acc1e474
Gerard Albajar <gerard.albajar@gmail.com>
2025-09-09 10:19:48 +0200

    Improve SEO

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,19 @@ import react from '@astrojs/react';
 
 export default defineConfig({
     site: 'https://www.edena.es',
+    i18n: {
+        defaultLocale: 'es',
+        locales: ['es', 'en'],
+        routing: {
+            prefixDefaultLocale: false
+        },
+        fallback: {
+            en: 'es'
+        }
+    },
+    redirects: {
+        '/': '/es'
+    },
     vite: {
         plugins: [tailwindcss()]
     },
