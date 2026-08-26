b3e288195fc1bf3fc92fc2c1af3163731a40797a
Gerard Albajar <gerard.albajar@gmail.com>
2026-08-26 12:40:26 +0200

    Add DownloadAppButtons component to Footer and Hero sections, update app download links to use constants for iOS and Android

---
+++ src/constants/app-stores.ts
@@ -0,0 +1,3 @@
+export const APP_STORE_URL = "https://apps.apple.com/es/app/edena/id6784446492";
+export const GOOGLE_PLAY_URL =
+    "https://play.google.com/store/apps/details?id=com.edena.app&utm_source=emea_Med";
